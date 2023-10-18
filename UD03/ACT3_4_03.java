67e254da41b7e191a1b03c47d1e896272a9cc2ab
unknown <bvives@iesemilidarder.com>
2023-10-18 07:23:07 +0200

    Initial

---
+++ UD03/ACT3_4_03.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ACT3_4_03 {
+/**
+ *
+ * @author Tomeu Vives
+ */
+    public static void main(String[] args) {
+		final int NALUMNES = 5;
+		final int NNOTES = 10;
+		final int MIN = 0;
+		final int MAX = 10;
+        Scanner scanner = new Scanner(System.in);
+		String[] alumnes = new String [NALUMNES];
+		int[][] notes = new int[NALUMNES][NNOTES];
+		
+		for (int i=0; i<alumnes.length; i++) {
+			System.out.print("Nom alumne [" + (i+1) + "]: ");
+			alumnes[i] = scanner.next();
+        }
+		
+		for (int i=0; i<notes.length; i++) {
+			for (int j=0; j<notes[i].length; j++) {
+				notes[i][j] = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
+			}
+		}
+
+		System.out.println("Les notes dels alumnes són: ");
+		for (int i=0; i<notes.length; i++) {
+			System.out.print(alumnes[i] + ": ");
+			for (int j=0; j<notes[i].length; j++) {
+				System.out.print(notes[i][j] + " ");
+			}
+			System.out.println();
+        }
+
+        scanner.close();
+    }
+}
