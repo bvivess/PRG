28b1254ce64eafd1d803c9a4a692894ce6f30640
unknown <bvives@iesemilidarder.com>
2023-10-18 07:24:47 +0200

    Initial

--- UD03/ACT3_4_03.java
+++ UD03/ACT3_4_03.java
@@ -13,18 +13,21 @@ public class ACT3_4_03 {
         Scanner scanner = new Scanner(System.in);
 		String[] alumnes = new String [NALUMNES];
 		int[][] notes = new int[NALUMNES][NNOTES];
-		
+
+		// Llegir no alumnes
 		for (int i=0; i<alumnes.length; i++) {
 			System.out.print("Nom alumne [" + (i+1) + "]: ");
 			alumnes[i] = scanner.next();
         }
 		
+		// Emplenar aleatòriament les notes de cada alumne
 		for (int i=0; i<notes.length; i++) {
 			for (int j=0; j<notes[i].length; j++) {
 				notes[i][j] = MIN + (int) (Math.random() * ((MAX - MIN) + 1));
 			}
 		}
 
+		// Mostrar les notes per cada alumne
 		System.out.println("Les notes dels alumnes són: ");
 		for (int i=0; i<notes.length; i++) {
 			System.out.print(alumnes[i] + ": ");
