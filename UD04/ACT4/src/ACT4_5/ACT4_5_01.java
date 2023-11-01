b740294a89857f53a85b77244f93f367dae213c9
bvivess <bvives@iesemilidarder.com>
2023-11-01 23:31:03 +0100

    Initial

--- UD04/ACT4/src/ACT4_5/ACT4_5_01.java
+++ UD04/ACT4/src/ACT4_5/ACT4_5_01.java
@@ -13,8 +13,33 @@ public class ACT4_5_01 {
     static int SIMBOL_FULLA = 1;
     static int SIMBOL_CUC = 9;
     static int[][] tauler;
-    static int[] cuc;
-    static int accio;
+    static int[] cuc; // x,y del cuc
+    static int accio; // (x,y) de les fulles
+    
+    public static void main(String[] args) {
+        final int NTAULER=UtilitatsConsola.llegirSencer("Mida del tauler: ");
+        tauler = new int[NTAULER][NTAULER];
+        cuc = new int[2];
+
+        emplenaTauler(tauler,cuc);
+        
+        do {
+            UtilitatsMatrius.mostrarMatriu(tauler);
+            accio=UtilitatsConsola.llegirSencer("ACCIONS: 8:ALT, 4:ESQUERRA, 6:DRETA, 2:BAIX; 0:SORTIR: ");
+            
+            cambiarPosicio(tauler, cuc, accio);
+
+        } while (accio!=0);
+    }
+    
+    public static void emplenaTauler(int[][] tauler, int[] cuc) {
+        final int minim = 0, maxim = tauler.length-1;
+        // Genera posició cuc
+        cuc[0]=(minim + (int) (Math.random() * (maxim - minim + 1)));
+        cuc[1]=(minim + (int) (Math.random() * (maxim - minim + 1)));
+        // Situa cuc en el tauler
+        tauler[cuc[0]][cuc[1]] = SIMBOL_CUC;
+    }
     
     public static void cambiarPosicio(int[][] tauler, int[] cuc, int car) {
         int mida = tauler.length;
@@ -33,20 +58,4 @@ public class ACT4_5_01 {
         tauler[cuc[0]][cuc[1]] = SIMBOL_CUC;
     }
     
-    public static void main(String[] args) {
-        final int MIDA=UtilitatsConsola.llegirSencer("Mida del tauler: ");
-        tauler = UtilitatsMatrius.generaMatriu(MIDA, 0, 0);
-        cuc = UtilitatsArrays.generaArray(2,0,MIDA-1);  // situa el cuc en una coor. inicial
-
-        tauler[cuc[0]][cuc[1]] = SIMBOL_CUC;
-        UtilitatsArrays.mostraArray(cuc);
-        
-        do {
-            UtilitatsMatrius.mostrarMatriu(tauler);
-            accio=UtilitatsConsola.llegirSencer("ACCIONS: 8:ALT, 4:ESQUERRA, 6:DRETA, 2:BAIX; 0:SORTIR: ");
-            
-            cambiarPosicio(tauler, cuc, accio);
-
-        } while (accio!=0);
-    }
 }
--- UD04/ACT4/src/ACT4_5/ACT4_5_02.java
+++ UD04/ACT4/src/ACT4_5/ACT4_5_02.java
@@ -14,7 +14,47 @@ public class ACT4_5_02 {
     static int SIMBOL_CUC = 9;
     static int[][] tauler;
     static int[] cuc;
+    static int[][] fulles;
     static int accio;
+    
+     public static void main(String[] args) {
+        final int NTAULER=UtilitatsConsola.llegirSencer("Mida del tauler: ");
+        final int NFULLES=UtilitatsConsola.llegirSencer("Nombre de fulles: ");
+        // 
+        tauler = new int[NTAULER][NTAULER];
+        cuc = new int[2];
+        //
+        fulles = new int[NFULLES][2];
+
+       
+        emplenaTauler(tauler, cuc, fulles);
+        UtilitatsArrays.mostraArray(cuc);
+        
+        do {
+            UtilitatsMatrius.mostrarMatriu(tauler);
+            accio=UtilitatsConsola.llegirSencer("ACCIONS: 8:ALT, 4:ESQUERRA, 6:DRETA, 2:BAIX; 0:SORTIR: ");
+            
+            cambiarPosicio(tauler, cuc, accio);
+
+        } while (accio!=0);
+    }
+     
+    public static void emplenaTauler(int[][] tauler, int[] cuc, int[][] fulles) {
+        final int minim = 0, maxim = tauler.length-1;
+        // Genera posició cuc
+        cuc[0]=(minim + (int) (Math.random() * (maxim - minim + 1)));
+        cuc[1]=(minim + (int) (Math.random() * (maxim - minim + 1)));
+        // Situa cuc en el tauler
+        tauler[cuc[0]][cuc[1]] = SIMBOL_CUC;
+        // Genera posició fulles
+        for (int i=0; i<fulles.length; i++) {
+            fulles[i] = UtilitatsArrays.generaArray(2,0, tauler.length-1); //
+        }
+        // Situa fulles en el tauler
+        for (int i=0; i<fulles.length; i++) {
+            tauler[ fulles[i][0]] [fulles[i][1] ] = SIMBOL_FULLA;
+        }
+    }
     
     public static void cambiarPosicio(int[][] tauler, int[] cuc, int accio) {
         final int MIDA = tauler.length;
@@ -33,20 +73,4 @@ public class ACT4_5_02 {
         tauler[cuc[0]][cuc[1]] = SIMBOL_CUC;
     }
     
-    public static void main(String[] args) {
-        final int MIDA=UtilitatsConsola.llegirSencer("Mida del tauler: ");
-        tauler = UtilitatsMatrius.generaMatriu(MIDA, SIMBOL_BUIT, SIMBOL_FULLA);
-        cuc = UtilitatsArrays.generaArray(2,0,MIDA-1);  // situa el cuc en una coor. inicial
-
-        tauler[cuc[0]][cuc[1]] = SIMBOL_CUC;
-        UtilitatsArrays.mostraArray(cuc);
-        
-        do {
-            UtilitatsMatrius.mostrarMatriu(tauler);
-            accio=UtilitatsConsola.llegirSencer(" | 8:ALT, 4:ESQUERRA, 6:DRETA, 2:BAIX; 0:SORTIR: ");
-            
-            cambiarPosicio(tauler, cuc, accio);
-
-        } while (accio!=0);
-    }
 }
