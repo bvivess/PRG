47c568d30b0f271432158c2b69f858f5f41d39d9
bvivess <bvives@iesemilidarder.com>
2023-11-20 07:17:21 +0100

    Initial

--- UD06/ACT6/src/ACT6_1/Modul.java
+++ UD06/ACT6/src/ACT6_1/Modul.java
@@ -15,6 +15,7 @@ public class Modul {
         comptadorModuls = 1;
     }
     
+    // Constructors
     public Modul(String nom, int horesSetmanals) throws Exception {
         if (nom == null || nom.length() == 0){
             throw new Exception("El nom no pot ser nul");
@@ -25,51 +26,32 @@ public class Modul {
         this.horesSetmanals = horesSetmanals;
         
     }
-    /**
-     * @return the idModul
-     */
+    
+    // Getters i Setters
     public int getIdModul() {
         return idModul;
     }
 
-    /**
-     * @return the nom
-     */
     public String getNom() {
         return nom;
     }
 
-    /**
-     * @return the horesSetmanals
-     */
     public int getHoresSetmanals() {
         return horesSetmanals;
     }
 
-    /**
-     * @return the comptadorModuls
-     */
     public static int getComptadorModuls() {
         return comptadorModuls++;
     }
 
-    /**
-     * @param idModul the idModul to set
-     */
     public void setIdModul(int idModul) {
         this.idModul = idModul;
     }
 
-    /**
-     * @param nom the nom to set
-     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    /**
-     * @param horesSetmanals the horesSetmanals to set
-     */
     public void setHoresSetmanals(int horesSetmanals) {
         this.horesSetmanals = horesSetmanals;
     }
