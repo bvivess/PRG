923ef0d081a989eb0a40cf5bed2ba8e870bc6d50
bartomeu vives <bvives@iesemilidarder.com>
2023-11-20 10:26:22 +0100

    Initial

--- UD06/ACT6/src/ACT6_1/Cicle.java
+++ UD06/ACT6/src/ACT6_1/Cicle.java
@@ -10,5 +10,51 @@ public class Cicle {
     private int numAlumnes;
     //
     private static int comptadorCicles = 1;
+
+    // Constructor
+    public Cicle(String nom, int numAlumnes) {
+        this.idCicle = getComptadorCicles();
+        this.nom = nom;
+        this.numAlumnes = numAlumnes;
+    }
+    
+    // Mòduls
+    public String mostraCicle() {
+        String text;
+        
+        text = "Cicle " + "ID=" + idCicle + ", nom=" + nom + ", numAlumnes=" + numAlumnes;
+        
+        return text;
+    }
+    
+    // Getters
+    public int getIdCicle() {
+        return idCicle;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getNumAlumnes() {
+        return numAlumnes;
+    }
+
+    public static int getComptadorCicles() {
+        return comptadorCicles++;
+    }
+    
+    // Setters
+    public void setIdCicle(int idCicle) {
+        this.idCicle = idCicle;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setNumAlumnes(int numAlumnes) {
+        this.numAlumnes = numAlumnes;
+    }
     
 }
--- UD06/ACT6/src/ACT6_1/Modul.java
+++ UD06/ACT6/src/ACT6_1/Modul.java
@@ -12,4 +12,50 @@ public class Modul {
     //
     private static int comptadorModuls=1;
 
+    // Constructor
+    public Modul(String nom, int idCicle, int horesSetmanals) {    
+        this.idModul = getComptadorModuls();
+        this.nom = nom;
+        this.idCicle = idCicle;
+        this.horesSetmanals = horesSetmanals;
+    }
+
+    // Getters
+    public int getIdModul() {
+        return idModul;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getIdCicle() {
+        return idCicle;
+    }
+
+    public int getHoresSetmanals() {
+        return horesSetmanals;
+    }
+
+    public static int getComptadorModuls() {
+        return comptadorModuls++;
+    }
+
+    // Setters
+    public void setIdModul(int idModul) {
+        this.idModul = idModul;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setIdCicle(int idCicle) {
+        this.idCicle = idCicle;
+    }
+
+    public void setHoresSetmanals(int horesSetmanals) {
+        this.horesSetmanals = horesSetmanals;
+    }
+    
 }
