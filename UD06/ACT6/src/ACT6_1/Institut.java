97dfdbd9518e00d4e0f32fb8e168c7e0322fa2be
bvivess <bvives@iesemilidarder.com>
2023-11-20 07:11:05 +0100

    Initial

--- UD06/ACT6/src/ACT6_1/Institut.java
+++ UD06/ACT6/src/ACT6_1/Institut.java
@@ -10,25 +10,34 @@ public class Institut {
 
     public static void main (String[] args) {
         try {
-            ArrayList<Modul> moduls;
+            ArrayList<Modul> moduls = new ArrayList<>();
             
-            Cicle cicle = new Cicle("Desenvolupament Aplicacions Web", 25);
             Modul modul1 = new Modul("Programació", 7);
             Modul modul2 = new Modul("Gestió de Base de Dades", 8);
             Modul modul3 = new Modul("Llenguatge de Marques", 4);
             Modul modul4 = new Modul("Sistemes d'Informació", 8);
             Modul modul5 = new Modul("Entorns de Desenvolupament", 4);
             
+            /* Alta del Cicle i dels mòduls segons el contructor1
+            Cicle cicle = new Cicle("Desenvolupament Aplicacions Web", 25);
             cicle.afegeixModul(modul1);
             cicle.afegeixModul(modul2);
             cicle.afegeixModul(modul3);
             cicle.afegeixModul(modul4);
             cicle.afegeixModul(modul5);
+            */
             
+            // Alta del Cicle i dels mòduls segons el contructor2
+            moduls.add(modul1);
+            moduls.add(modul2);
+            moduls.add(modul3);
+            moduls.add(modul4);
+            moduls.add(modul5);
+            Cicle cicle = new Cicle("Desenvolupament Aplicacions Web", 25, moduls);
             
             System.out.println(cicle.mostraCicle() );
             if (cicle.eliminarModul("Programació")) {
-                System.out.println("Programació eliminat");
+                System.out.println("'Programació' eliminat");
             }
             System.out.println(cicle.mostraCicle() );
         } catch (Exception e) {
