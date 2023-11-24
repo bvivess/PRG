54a0aa6af515f9d3f7774cd9e6fc981e96e689cc
bvivess <bvives@iesemilidarder.com>
2023-11-24 08:45:04 +0100

    Initial

--- UD05/ACT5/src/ACT5_1/ACT5_1_2.java
+++ UD05/ACT5/src/ACT5_1/ACT5_1_2.java
@@ -7,13 +7,21 @@ package ACT5_1;
 public class ACT5_1_2 {
     public static void main(String[] args) {
         int[] array = {0,1,2,3,4,5,6,7,8,9};
+        int posicio = UtilitatsConsola.llegirSencer("Introdueixi la posició de l'array a llegir (0-" + (array.length-1) + "): ");
         
         try {
-            for (int i=0; i<=array.length; i++)
-                System.out.println(array[i]);
-        } catch (Exception e) {
+            
+            System.out.println(array[posicio]);
+            
+        } catch (IndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
             System.out.println("S'ha excedit la longitud de l'array");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Error d'execució");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Error generalyy");
         }
     }
 }
