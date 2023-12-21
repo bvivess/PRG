fd122169bb168ed81ca29dd144d3a83c5bad0986
bvivess <bvives@iesemilidarder.com>
2023-12-21 18:12:12 +0100

    Initial

---
+++ UD06/ACT6/src/ACT6_2/Client.java
@@ -0,0 +1,24 @@
+package ACT6_2;
+
+/**
+ *
+ * @author T.Vives
+ */
+public class Client {
+    // Atributs
+    public String llinatge1;
+    public String llinatge2;
+    public String nom;
+    public Compte compte;
+
+    // Mètodes
+    public void realitzaTransaccio(double quantitat) {
+        // ha de modificar el saldo del compte del client
+        if (quantitat > 0) {
+            compte.ingresar(quantitat);
+        } else if (quantitat < 0) {
+            compte.reintegrar(Math.abs(quantitat));  // valor en positiu de 'quantitat'
+        }
+    }
+}
+
---
+++ UD06/ACT6/src/ACT6_2/Main.java
@@ -0,0 +1,38 @@
+package ACT6_2;
+
+/**
+ *
+ * @author T.Vives
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            // Declarar i instanciar un compte bancari
+            Compte compte1 = new Compte();
+            compte1.banc = "0149";
+            compte1.oficina = "0001";
+            compte1.dc = "31";
+            compte1.numeroCompte = "1234567890";
+
+            // // Declarar i instanciar un client
+            Client client1 = new Client();
+            client1.nom = "Joan";
+            client1.llinatge1 = "Soler";
+            client1.llinatge2 = "Matemales";
+            client1.compte = compte1;
+
+            // Realizar transacciones
+            client1.realitzaTransaccio(500.0);
+            client1.realitzaTransaccio((-200.0));
+
+            // Mostrar saldo final
+            System.out.println("Client " + client1.nom + " " + client1.llinatge1 + " amb SALDO " + 
+                               client1.compte.saldo );
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+}
+
