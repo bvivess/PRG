13b9ac36052c3891ef23c8ef229da84dcb2e28e1
bvivess <bvives@iesemilidarder.com>
2024-01-08 19:11:04 +0100

    Initial

--- UD06/ACT6/src/ACT6_0C/Casella.java
+++ UD06/ACT6/src/ACT6_0C/Casella.java
@@ -5,13 +5,8 @@ package ACT6_0C;
  * @author winadmin
  */
 public class Casella {
-    private int fila;
-    private int columna;
-
-    public Casella(int fila, int columna) {
-        this.fila = fila;
-        this.columna = columna;
-    }
+    public int fila;
+    public int columna;
 
     // ...
 }
--- UD06/ACT6/src/ACT6_0C/Main.java
+++ UD06/ACT6/src/ACT6_0C/Main.java
@@ -1,5 +1,7 @@
 package ACT6_0C;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author winadmin
@@ -7,10 +9,18 @@ package ACT6_0C;
 public class Main {
 
     public static void main(String[] args) {
-        // int[][] tauler = new int[8][8];
-        Tauler tauler = new Tauler();
-        // Casella[][] tauler;
-        
-        tauler.mostraTauler();
+        int[][] tauler = new int[8][8];
+        ArrayList<Casella> cuc = new ArrayList<>();  // cada segment del cuc està és un element de l'ArrayList	
+
+        Casella segment1 = new Casella();
+        segment1.fila = 2;
+        segment1.columna = 3;
+        cuc.add(segment1);  // l'objecte 'cuc' té un primer segment en la 'fila'=2 i 'columna'=3
+        Casella segment2 = new Casella();
+        segment2.fila = 2;
+        segment2.columna = 4;
+        cuc.add(segment2);  // l'objecte 'cuc' té un segon segment en la 'fila'=2 i 'columna'=4
+        // ...
     }
+
 }
