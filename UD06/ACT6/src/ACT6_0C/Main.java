6e0bfb2709fa4b5c615bff42d99a3fa1ad0b939b
bartomeu vives <bvives@iesemilidarder.com>
2024-01-08 12:30:34 +0100

    Initial

--- UD06/ACT6/src/ACT6_0C/Main.java
+++ UD06/ACT6/src/ACT6_0C/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         // int[][] tauler = new int[8][8];
         Tauler tauler = new Tauler();
+        // Casella[][] tauler;
         
         tauler.mostraTauler();
     }
