e9c8a2278722c00ffb85abba624e17410f60f3db
bvivess <bvives@iesemilidarder.com>
2024-01-10 07:40:52 +0100

    Initial

---
+++ UD06/ACT6/src/ACT6_0D/Empleat.java
@@ -0,0 +1,20 @@
+package ACT6_0D;
+
+/**
+ *
+ * @author T.Vives
+ */
+public class Empleat {
+    public String nom;
+    public static double souBase;
+    public double souVariable;
+
+    // Mètode estàtic 
+    public static void modificaSouBase(double nouSouBase) {
+        souBase = nouSouBase;
+    }
+    // Mètode no estàtic
+    public double calcularSou() {
+        return souBase + souVariable;
+    }
+}
---
+++ UD06/ACT6/src/ACT6_0D/Main.java
@@ -0,0 +1,19 @@
+package ACT6_0D;
+
+/**
+ *
+ * @author T.Vives
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Empleat e1 = new Empleat(); e1.nom = "Joan Martínez"; e1.souVariable = 1000;
+        Empleat e2 = new Empleat(); e2.nom = "Pere Martell"; e2.souVariable = 500; 
+        Empleat.modificaSouBase(2000);  // també 'Empleat.souBase = 2000;'
+        System.out.println(e1.nom + " " + Empleat.souBase + ", " + e1.souVariable + "=" + e1.calcularSou());
+        System.out.println(e2.nom + " " + Empleat.souBase + ", " + e2.souVariable + "=" + e2.calcularSou());
+    }
+}
