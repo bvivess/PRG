53f3895de6d44962c6c16f72b017a9176e6adb92
bvivess <bvives@iesemilidarder.com>
2024-01-13 17:57:47 +0100

    Initial

--- UD06/ACT6/src/ACT6_2B/Compte.java
+++ UD06/ACT6/src/ACT6_2B/Compte.java
@@ -7,22 +7,13 @@ import ACT6_2A.*;
  * @author T.Vives
  */
 public class Compte {
-    private String banc;  // codi banc: String 4 posicions
-    private String oficina;  // codi oficina: String 4 posicions
-    private String dc;  // digits control: String 2 posicions
-    private String numCompte; // núm compte: String 10 posicions
-    private double saldo;  // no és possible un valor negatiu per aquest atribut
+    // El codi d'un compte està format per 4 atribut: banc+oficina+dc+numCompte
+    public String banc;  // codi banc: String 4 posicions
+    public String oficina;  // codi oficina: String 4 posicions
+    public String dc;  // digits control: String 2 posicions
+    public String numCompte; // núm compte: String 10 posicions
+    public double saldo;  // no és possible un valor negatiu per aquest atribut
 
-    // Constructor
-    public Compte(String banc, String oficina, String dc, String numCompte, double saldo) {
-        this.banc = banc;
-        this.oficina = oficina;
-        this.dc = dc;
-        this.numCompte = numCompte;
-        this.saldo = saldo;
-    }
-    
-    // Mètodes específics
     public void ingresa(double quantitat) {
         // ha d'incrementar la 'quantitat' al saldo del compte
         saldo += quantitat;
@@ -39,46 +30,4 @@ public class Compte {
             System.out.println("Saldo insuficient " + saldo + " en el compte " + banc + "-" + oficina + "-" + dc + "-" + numCompte);
         }
     }
-
-    public String getBanc() {
-        return banc;
-    }
-
-    public void setBanc(String banc) {
-        this.banc = banc;
-    }
-
-    public String getOficina() {
-        return oficina;
-    }
-
-    public void setOficina(String oficina) {
-        this.oficina = oficina;
-    }
-
-    public String getDc() {
-        return dc;
-    }
-
-    public void setDc(String dc) {
-        this.dc = dc;
-    }
-
-    public String getNumCompte() {
-        return numCompte;
-    }
-
-    public void setNumCompte(String numCompte) {
-        this.numCompte = numCompte;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    
-    
 }
