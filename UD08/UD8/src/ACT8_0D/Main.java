3386e04404f8972fc5bc0c9621dd74a5d286731f
bvivess <bvives@iesemilidarder.com>
2024-01-23 08:25:09 +0100

    Initial

--- UD08/UD8/src/ACT8_0D/Main.java
+++ UD08/UD8/src/ACT8_0D/Main.java
@@ -11,18 +11,18 @@ package ACT8_0D;
  */
 public class Main {
     public static void main(String[] args) {
-        Persona p1 = new Persona ("Joan",10);
-        Persona p2 = new Persona ("Pere",10);
+        Persona p1 = new Persona ("Maria",18);
+        Persona p2 = new Persona ("Pere",22);
         Persona p3 = p1; // iguala referències, no continguts
         
-        System.out.println(p1.toString());  // estudiant.mostraEstudiant()
+        System.out.println(p1.toString());  // persona.mostraPersona()
         System.out.println(p2.toString());
         System.out.println(p3.toString());
         System.out.println("p1-p2: " + p1.equals(p2));
         System.out.println("p1-p3: " + p1.equals(p3));
 
-        Estudiant e1 = new Estudiant ("Joan",10,"DAW");
-        Estudiant e2 = new Estudiant ("Pere",10,"DAW");
+        Estudiant e1 = new Estudiant ("Joan",20,"Programació");
+        Estudiant e2 = new Estudiant ("Aina",31,"Programació");
         Estudiant e3 = e1; // iguala referències, no continguts
         
         System.out.println(e1.toString());  // estudiant.mostraEstudiant()
