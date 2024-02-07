d5a6faf96ea66a0d763f5f6229f3c4398e19d6a5
unknown <bvives@iesemilidarder.com>
2024-02-07 18:41:10 +0100

    Initial

--- UD09/ACT9/src/ACT9_2B/Main.java
+++ UD09/ACT9/src/ACT9_2B/Main.java
@@ -11,44 +11,65 @@ public class Main {
         ArrayList<Persona> persones = new ArrayList<Persona>();
         ArrayList<Telefon> telefons = new ArrayList<Telefon>();
         
-        // Persona1
-        Persona persona = new Persona("Joan","1234", null, null, null);
-        persones.add(persona);
+        // Persones
+        carregaPersones(persones);
         
+        // Telefons
+        carregaTelefons(telefons, persones);
+        
+        // Encenent dispositius
+        encenDispositius(telefons);
+        
+        // Validacions
+        validaDispositius(telefons, persones);
+        validaPersones(telefons, persones);
+
+        // Apagant dispositius
+        apagaDispositius(telefons);
+    }
+    
+    private static void carregaPersones(ArrayList<Persona> persones) {
+        Persona persona;
+        
+        persona = new Persona("Joan","1234", null, null, null);
+        persones.add(persona);
         // Persona2
         persona = new Persona("Gemma",null, "Teclat2", null, null);
         persones.add(persona);
-        
         // Persona3
         persona = new Persona("Pere",null, null, "Emprempta3", null);
         persones.add(persona);
-        
         // Persona4
         persona = new Persona("Maria",null, null, null, "Rostre4");
         persones.add(persona);
-        
-        // Android
-        Telefon telefon = new Android("Samsung", persones.get(0),"1234", null, null);
+    }
+    
+    private static void carregaTelefons(ArrayList<Telefon> telefons, ArrayList<Persona> persones) {
+        Telefon telefon;
+        telefon = new Android("Samsung", persones.get(0),"1234", null, null);
         telefons.add(telefon);
-        
+        // Android
         telefon = new Android("Xiaomi", persones.get(1),null, "Teclat2", null);
         telefons.add(telefon);
-        
         // IPhone8
         telefon = new IPhone8("Apple",persones.get(2), null, "Emprempta3");
         telefons.add(telefon);
-        
         // IPhone10
         telefon = new IPhone10("Apple",persones.get(3),null, null, "Rostre4");
         telefons.add(telefon);
-        
-        // Encenent dispositius
-        System.out.print("\n");
-        for (Telefon t : telefons) {
-            t.setOnOff(true);
-            System.out.println("Encenent " + t.toString() + "...");
+    }
+    
+    private static void validaDispositius(ArrayList<Telefon> telefons, ArrayList<Persona> persones) {
+        // Validacions Persona a Telefon
+        for (Persona p : persones) {
+            for (Telefon t : telefons) {
+                System.out.print("\t" + p.getNom() + " " + t.validaPatrons(p) + " ");
+            }
+            System.out.print("\n" );
         }
-
+    }
+    
+    private static void validaPersones(ArrayList<Telefon> telefons, ArrayList<Persona> persones) {
         // Validacions Telefon a Persona
         System.out.print("\n");
         for (Persona p : persones) {
@@ -57,22 +78,19 @@ public class Main {
             }
             System.out.print("\n");
         }
-
-        // Validacions Persona a Telefon
-        System.out.print("\n");
+    }
+    
+    private static void encenDispositius(ArrayList<Telefon> telefons) {
         for (Telefon t : telefons) {
-            for (Persona p : persones) {
-                System.out.print("\t" + t.toString() + " " +  p.validaPatrons(t));
-            }
-            System.out.print("\n");
-        }
-        
-        // Apagant dispositius
-        System.out.print("\n");
-        for (Telefon t : telefons) {
-            t.setOnOff(false);
             System.out.println("Apagant " + t.toString() + "...");
+            t.on();
         }
-        
+    }
+    
+    private static void apagaDispositius(ArrayList<Telefon> telefons) {
+        for (Telefon t : telefons) {
+            System.out.println("Apagant " + t.toString() + "...");
+            t.off();
+        }
     }
 }
