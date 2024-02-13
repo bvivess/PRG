535036c1deee3c645a82138f26056dcd52df2d69
unknown <bvives@iesemilidarder.com>
2024-02-14 00:12:14 +0100

    Initial

--- UD11/ACT11/src/ACT11_1/Main.java
+++ UD11/ACT11/src/ACT11_1/Main.java
@@ -15,30 +15,19 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Mòduls DAW
-        Persona persona1 = new Persona("Joan",23);
-        Persona persona2 = new Persona("Gemma",30);
-        Persona persona3 = new Persona("Pere",36);
-        Persona persona4 = new Persona("Maria",18);
-        
+        // Persones
         List<Persona> persones = new ArrayList<>();
-        persones.add(persona1);
-        persones.add(persona2);
-        persones.add(persona3);
-        persones.add(persona4);
-           
-        Modul modul1 = new Modul("Programació",7, persones);
-        Modul modul2 = new Modul("Gestió de Base de Dades",8, persones);
-        Modul modul3 = new Modul("Llenguatge de Marques",4, persones);
-        Modul modul4 = new Modul("Sistemes d'Informació",7, persones);
-        Modul modul5 = new Modul("Entorns de Desenvolupament",3, persones);
-         
+        persones.add(new Persona("Joan",23));
+        persones.add(new Persona("Gemma",30));
+        persones.add(new Persona("Pere",36));
+        persones.add(new Persona("Maria",18));
+        // Mòduls
         Map<Integer, Modul> moduls = new HashMap<>();
-        moduls.put(1, modul1);
-        moduls.put(2, modul2);
-        moduls.put(3, modul3);
-        moduls.put(4, modul4);
-        moduls.put(5, modul5);
+        moduls.put(1, new Modul("Programació",7, persones));
+        moduls.put(2, new Modul("Gestió de Base de Dades",8, persones));
+        moduls.put(3, new Modul("Llenguatge de Marques",4, persones));
+        moduls.put(4, new Modul("Sistemes d'Informació",7, persones));
+        moduls.put(5, new Modul("Entorns de Desenvolupament",3, persones));
         
         // Del mòdul amb clau=1, Nom del mòdul i a continuació Nom de la tercera persona matriculada
         System.out.println(moduls.get(1).nom + ": " + moduls.get(1).matricula.get(2).nom );
