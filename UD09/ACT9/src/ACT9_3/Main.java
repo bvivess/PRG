5d059f1f125efad2e7b3119b601dfe4f93f129d4
unknown <bvives@iesemilidarder.com>
2024-02-13 23:56:37 +0100

    Initial

--- UD09/ACT9/src/ACT9_3/Main.java
+++ UD09/ACT9/src/ACT9_3/Main.java
@@ -6,16 +6,12 @@ package ACT9_3;
  */
 public class Main {
     public static void main(String[] args) {
-        // Creació de concessionaris i els vehicles
+        // Creació de concessionaris i vehicles
         ConcessionariAutos concessionariAutos = new ConcessionariAutos();
-        concessionariAutos.setAutomobil(new Automobil("Renault", "Megane", 25000.0, "Turisme", 4, 500.0));
-        concessionariAutos.setAutomobil(new Automobil("Honda", "Civic", 22000.0, "Turisme", 4, 450.0));
-        concessionariAutos.setCamio(new Camio("Pegasus", "Brabus",15000,"Trucker", 5000.0));
-        concessionariAutos.setCamio(new Camio("Ebro", "Santana",10000, "Truck", 500.0));
+        creaConcessionariAutos(concessionariAutos);
 
         ConcessionariMotos concessionariMotos = new ConcessionariMotos();
-        concessionariMotos.setMotocicleta(new Motocicleta("Vespa", "Primavera", 2000.0, "Scotter", 125));
-        concessionariMotos.setMotocicleta(new Motocicleta("Honda", "CBR", 15000.0, "Deportiva", 600));
+        creaConcessionariMotos(concessionariMotos);
 
         // Mostrar resultats
         System.out.println(concessionariAutos.toString());
@@ -23,4 +19,17 @@ public class Main {
         System.out.println("Ingressos del Concesionari de Autos: " + concessionariAutos.calculaIngressos());
         System.out.println("Ingressos del Concesionari de Motos: " + concessionariMotos.calculaIngressos());
     }
+    
+    private static void creaConcessionariAutos(ConcessionariAutos concessionariAutos) {
+        concessionariAutos.setAutomobil(new Automobil("Renault", "Megane", 25000.0, "Turisme", 4, 500.0));
+        concessionariAutos.setAutomobil(new Automobil("Honda", "Civic", 22000.0, "Turisme", 4, 450.0));
+        concessionariAutos.setCamio(new Camio("Pegasus", "Brabus",15000,"Trucker", 5000.0));
+        concessionariAutos.setCamio(new Camio("Ebro", "Santana",10000, "Truck", 500.0));
+    }
+    
+    private static void creaConcessionariMotos(ConcessionariMotos concessionariMotos) {
+        concessionariMotos.setMotocicleta(new Motocicleta("Vespa", "Primavera", 2000.0, "Scotter", 125));
+        concessionariMotos.setMotocicleta(new Motocicleta("Honda", "CBR", 15000.0, "Deportiva", 600));
+    }
+
 }
