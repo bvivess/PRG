54547624f68aadd94c5f49c9e54a28e66b5adf99
unknown <bvives@iesemilidarder.com>
2024-02-28 08:58:00 +0100

    Initial

--- EXAMENS/src/EX_UD09_II_A/Main.java
+++ EXAMENS/src/EX_UD09_II_A/Main.java
@@ -13,17 +13,13 @@ public class Main {
         Article article = new Article("Pa moreno", 0.80, 1.75, new TipusIva("Normal",0.21));
         ArticlesPerQuantitat articlePerQuantitat = new ArticlesPerQuantitat(article, 10);
         
-        // Stock
-        ArrayList<ArticlesPerQuantitat> stock = new ArrayList<>();
-        stock.add(articlePerQuantitat);
-        
-        // Carrito
-        ArrayList<ArticlesPerQuantitat> carrito = new ArrayList<>();
-        carrito.add(articlePerQuantitat);
+        // Articles: Stock o Carrito
+        ArrayList<ArticlesPerQuantitat> articles = new ArrayList<>();
+        articles.add(articlePerQuantitat);
         
         // Supermercat i Client
-        Supermercat supermercat = new Supermercat("Lidl", stock);
-        Client client = new Client("Bernat Maimó", carrito );
+        Supermercat supermercat = new Supermercat("Lidl", articles);
+        Client client = new Client("Bernat Maimó", articles );
         
     }
 }
