42fc1e5432a84523b89235b18fb767ef0402a4f6
unknown <bvives@iesemilidarder.com>
2024-02-28 08:08:39 +0100

    Initial

--- EXAMENS/src/EX_UD09_II_A/Main.java
+++ EXAMENS/src/EX_UD09_II_A/Main.java
@@ -12,9 +12,11 @@ public class Main {
         // Articles
         Article article = new Article("Pa moreno", 0.80, 1.75, new TipusIva("Normal",0.21));
         ArticlesPerQuantitat articlePerQuantitat = new ArticlesPerQuantitat(article, 10);
+        
         // Stock
         ArrayList<ArticlesPerQuantitat> stock = new ArrayList<>();
         stock.add(articlePerQuantitat);
+        
         // Carrito
         ArrayList<ArticlesPerQuantitat> carrito = new ArrayList<>();
         carrito.add(articlePerQuantitat);
