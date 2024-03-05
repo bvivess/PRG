1fd5739e647c1692d1e4ad9adbe95a0b5e4f1e04
unknown <bvives@iesemilidarder.com>
2024-03-05 16:29:48 +0100

    Initial

--- UD12/UD12/src/ACT12_0A/Main.java
+++ UD12/UD12/src/ACT12_0A/Main.java
@@ -1,16 +1,26 @@
 package ACT12_0A;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author winadmin
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nomArxiu = "c:\\temp\\arxiu.txt";
+        try (BufferedReader lector = new BufferedReader(new FileReader(nomArxiu))) {
+            String linnia;
+            while ((linnia = lector.readLine()) != null) { // Llegir cada línia del fitxer
+                System.out.println(linnia);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
     
 }
---
+++ UD12/UD12/src/ACT12_0B/Persona.java
@@ -0,0 +1,28 @@
+package ACT12_0B;
+
+import java.util.List;
+
+/**
+ *
+ * @author winadmin
+ */
+public class Persona {
+    private String nom;
+    private int edat;
+    private List<String> aficions;
+
+    // Getters y Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+
+    public List<String> getAficions() {
+        return aficions;
+    }
+    
+}
+
