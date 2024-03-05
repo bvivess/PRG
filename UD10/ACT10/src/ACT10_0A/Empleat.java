4494bb48833477f5a7f7f3ec6d593337fbbe0ba6
unknown <bvives@iesemilidarder.com>
2024-03-05 08:27:02 +0100

    Initial

--- UD10/ACT10/src/ACT10_0A/Empleat.java
+++ UD10/ACT10/src/ACT10_0A/Empleat.java
@@ -4,16 +4,16 @@ public class Empleat {
     private String nom;
     private String llinatge;
     
-    private static Empleat empleat;  // per assegurar que sols hi ha un empleat
+    private static Empleat empleat;  // 'static' per assegurar que sols hi ha un empleat
     
     // Constructor en 'private'
-    private Empleat(String nom, String llinatge) {  // per assegurar que no es crida des de fora
+    private Empleat(String nom, String llinatge) {  // 'private' per assegurar que no es crida des de fora
         this.nom = nom;
         this.llinatge = llinatge;
     }
     
     // Pseudo-constructor
-    public static Empleat getEmpleat(String nom, String llinatge) {
+    public static Empleat getEmpleat(String nom, String llinatge) {  // torna el valor de l'atribut 'empleat'
         if (empleat == null)  // si no s'ha instanciat ?
             empleat = new Empleat(nom, llinatge);
         return empleat;
