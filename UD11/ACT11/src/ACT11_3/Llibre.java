7591d0605ed0a5717293cf7b5685e613b3ddb67f
unknown <bvives@iesemilidarder.com>
2024-03-21 15:34:40 +0100

    Initial

--- UD11/ACT11/src/ACT11_3/Llibre.java
+++ UD11/ACT11/src/ACT11_3/Llibre.java
@@ -1,15 +1,19 @@
 package ACT11_3;
 
+import java.util.Objects;
+
 /**
  *
  * @author winadmin
  */
 public class Llibre {
+    private int idLlibre;
     private String titol;
     private String autor;
     private int anyPublicacio;
 
-    public Llibre(String titol, String autor, int anyPublicacio) {
+    public Llibre(int idLlibre, String titol, String autor, int anyPublicacio) {
+        this.idLlibre = idLlibre;
         this.titol = titol;
         this.autor = autor;
         this.anyPublicacio = anyPublicacio;
@@ -27,6 +31,30 @@ public class Llibre {
         return anyPublicacio;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.idLlibre);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Llibre other = (Llibre) obj;
+        return Objects.equals(this.idLlibre, other.idLlibre);
+    }
+
+    
+    
     @Override
     public String toString() {
         return titol + " de " + autor + " (" + anyPublicacio + ")";
