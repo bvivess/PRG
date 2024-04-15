ddc85092e18c011b1a540dd7f2a154a672f41fc6
unknown <bvives@iesemilidarder.com>
2024-04-15 23:12:20 +0200

    Initial

---
+++ EXAMENS/src/EX_UD11_1/Tasca.java
@@ -0,0 +1,31 @@
+package EX_UD11_1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Tasca {
+    private LocalDate dia;
+    private String tarea;
+
+    public Tasca(LocalDate dia, String tarea) {
+        this.dia = dia;
+        this.tarea = tarea;
+    }
+
+    public LocalDate getDia() {
+        return dia;
+    }
+
+    public String getTarea() {
+        return tarea;
+    }
+
+    @Override
+    public String toString() {
+        return "Tasca{" + "dia=" + dia + ", tarea=" + tarea + '}';
+    }
+    
+    
+}
+
+
--- EXAMENS/src/EX_UD11_2/Main.java
+++ EXAMENS/src/EX_UD11_2/Main.java
@@ -1,36 +1,47 @@
-package ACT11_2B;
+package EX_UD11_2;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        Agenda agenda = new Agenda();
+        Map<LocalDate, Queue<Persona>> agenda = new TreeMap<>();
+        Queue<Persona> persones1 = new LinkedList<>();
+        persones1.offer(new Persona("Joan",27));
+        Queue<Persona> persones2 = new LinkedList<>();
+        persones2.offer(new Persona("Maria", 30));
+        persones2.offer(new Persona("Pep",31));
+        Queue<Persona> persones3 = new LinkedList<>();
+        persones3.offer(new Persona("Aina", 36));
+        persones3.offer(new Persona("Josep", 39));
+        persones3.offer(new Persona("Arnau",51));
         
         // Càrrega de l'agenda
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 01), LocalTime.of(10,00), "Tasca1");
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 01), LocalTime.of(12,00), "Tasca2");
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 01), LocalTime.of(18,00), "Tasca3");
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 02), LocalTime.of(10,00), "Tasca1");
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 03), LocalTime.of(10,00), "Tasca1");
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 03), LocalTime.of(12,00), "Tasca2");
+        agenda.put(LocalDate.of(2024, 1, 01), persones1);
+        agenda.put(LocalDate.of(2024, 1, 02), persones2);
+        agenda.put(LocalDate.of(2024, 1, 03), persones3);
 
-        System.out.println(agenda.toString());
+        for (LocalDate k : agenda.keySet()) {
+            Queue<Persona> valor = agenda.get(k);
+            System.out.println("Clau: " + k + ", Valor: " + valor.toString());
+        }
         
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 02), LocalTime.of(12,00), "Tasca2");
-        agenda.afegeixTasca(LocalDate.of(2024, 1, 02), LocalTime.of(18,00), "Tasca3");
-        System.out.println("-------------------------");
-        System.out.println(agenda.toString());      
+        // 1. Del mòdul amb clau='01/01/2024':  'nom' de la 'Persona' primera de la cua
+        System.out.println(agenda.get(LocalDate.of(2024, 1, 01)).poll().getNom());
         
-        // 
-        agenda.eliminaTasca(LocalDate.of(2024, 1, 01), LocalTime.of(10,00));
-        System.out.println("-------------------------");
-        System.out.println(agenda.toString());
-        System.out.println("-------------------------");
-        System.out.println("Tasca per 01/01/2024-12:00 - " +
-                            agenda.obteTasca(LocalDate.of(2024, 1, 01), LocalTime.of(12,00)));
-        System.out.println("Tasca per 02/01/2024-11:00 - " +
-                            agenda.obteTasca(LocalDate.of(2024, 1, 02), LocalTime.of(11,00)));
+        // 2. Del mòdul amb clau='02/01/2024':  'edat' de la 'Persona' primera de la cua
+        System.out.println(agenda.get(LocalDate.of(2024, 1, 02)).poll().getEdat());
+        
+        // 3. Del mòdul amb clau='03/01/2024':  Llista el 'nom' de cada 'Persona' de la cua
+        for (Persona p : agenda.get(LocalDate.of(2024, 1, 03))) {
+            System.out.println(p.getNom());
+        }
+        
     }
 }
 
