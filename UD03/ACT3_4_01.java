68d14effe3abfa8ffcd8fbca17d574c58593b3a4
unknown <bvives@iesemilidarder.com>
2024-10-29 08:47:13 +0100

    Initial

--- UD03/ACT3_4_01.java
+++ UD03/ACT3_4_01.java
@@ -1,54 +1,66 @@
 import java.util.Scanner;
 
-public class ACT3_4_02 {
+public class ACT3_4_01 {
 /**
  *
  * @author Tomeu Vives
  */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int total, min, max, suma = 0;
+        int numElements, min, max, total, suma;
 		int[][] matriu;
 		double mitjana;
 
-        System.out.print("Introduexi el núm elements: ");
-        total = scanner.nextInt();
-        matriu = new int[total][total];
+		// Demanar mida de la matriu
+        System.out.print("Introduexi el núm elements de la matriu: ");
+        numElements = scanner.nextInt();
+        matriu = new int[numElements][numElements];
 
-        // Llegim la matriu per teclat
+        // Llegim les dades de la matriu per teclat
         for (int i = 0; i < matriu.length; i++)
 			for (int j = 0; j < matriu[0].length; j++) {
 				System.out.print("matriu[" + i + "][" + j + "]: ");
 				matriu[i][j] = scanner.nextInt();
 			}
 
-        // Mostra la matriu y calcula el min, max i mitjana
-
-        max = matriu[0][0];
+        // Mostra la matriu 
+		System.out.println("La matriu és:");
+		for (int[] array : matriu) {
+			for (int valor : array) 
+				System.out.print( valor + " " );
+			System.out.println();
+		}
+			
+		// Calcula el min
         min = matriu[0][0];
+        for (int[] array : matriu)
+			for (int valor : array)
+				if (valor < min )
+					min = valor;
+
+				
+		// Calcula el max
+        max = matriu[0][0];
+        for (int[] array : matriu)
+			for (int valor : array)
+				if (valor > max)
+					max = valor;
+				
+		// Calcula el min, max i mitjana
+		total = 0; suma = 0;
         for (int[] array : matriu)
 			for (int valor : array) {
 				suma += valor;
-				if (valor > max)
-					max = valor;
-
-				if (valor < min)
-					min = valor;
+				total++;
 			}
-
         mitjana = (double) suma / total;
 
-        //System.out.println("[ ");
-        for (int[] array : matriu) {
-			for (int valor : array)
-				System.out.print(valor + " ");
-			System.out.println();
-        }
-		//System.out.println("]");
-        
-		System.out.println("Màxim: " + max);
-        System.out.println("Mínim: " + min);
-        System.out.println("Mitjana: " + mitjana);
+		
+		// Mostrar resultats
+		System.out.println("Els resultats són:");
+		System.out.println("\tMàxim: " + max);
+        System.out.println("\tMínim: " + min);
+        System.out.println("\tMitjana: " + mitjana);
 
         scanner.close();
     }
