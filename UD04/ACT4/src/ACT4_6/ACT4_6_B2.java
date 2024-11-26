f62a1e1265cb6d3ca5aa48f9c7fff9703efdc823
unknown <bvives@iesemilidarder.com>
2024-11-26 07:01:20 +0100

    Initial

--- UD04/ACT4/src/ACT4_6/ACT4_6_B2.java
+++ UD04/ACT4/src/ACT4_6/ACT4_6_B2.java
@@ -32,9 +32,9 @@ public class ACT4_6_B2 {
         boolean oktorre = true;
         
         do {
-            torre  = UtilitatsArrays.generaArray(2, 0, NTAULER-1); // genera posició de la fulla
+            torre  = UtilitatsArrays.generaArray(2, 0, NTAULER-1); // genera posició de la torre
 
-            if (tauler[torre[0]][torre[1]] == SIMBOL_TORRE) { // situa la fulla al tauler si la posició està  buida
+            if (tauler[torre[0]][torre[1]] == SIMBOL_TORRE) { // situa la torre en el tauler si la posició està no està ocupada per una altra torre
                 oktorre = false;
             }
         } while (!oktorre);
