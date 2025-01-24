bb1d18286999ba5d7110b0f8bf2a17cb9aa416e3
unknown <bvives@iesemilidarder.com>
2025-01-24 08:29:51 +0100

    Initial

--- UD07/UD7/src/ACT7_1A/Manager.java
+++ UD07/UD7/src/ACT7_1A/Manager.java
@@ -1,11 +1,29 @@
 package ACT7_1A;
 
+import java.util.ArrayList;
+
 class Manager extends Employee {
     private String deptName;
+    private ArrayList<Employee> staff;
 
     public Manager(int empId, String name, String ssn, double salary, String deptName) {
+        
         super(empId, name, ssn, salary);
         this.deptName = deptName;
+        this.staff = new ArrayList<>();
+    }
+    
+    public boolean addEmployee(Employee e) {
+        if (this.staff.contains(e))
+            return false;
+        else {
+            this.staff.add(e);
+            return true;
+        }
+    }
+    
+    public boolean removeEmployee(Employee e) {
+        return this.staff.remove(e);
     }
 
     public String getDeptName() {
@@ -13,7 +31,10 @@ class Manager extends Employee {
     }
     
     public String printManager() {
-        return "Manager {" + super.printEmployee() + ", deptName=" + this.deptName + "}";
+        String text = "Manager {" + super.printEmployee() + ", deptName=" + this.deptName;
+        for (Employee e : this.staff)
+            text = text + e.printEmployee();
+        return text + "}";
     }
     
 }
