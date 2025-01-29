d3616da611a00974825d1b1dea447deb4b59843c
bartomeu vives <bvives@iesemilidarder.com>
2025-01-29 17:00:33 +0100

    Initial

--- UD07/UD7/src/ACT7_1C/Main.java
+++ UD07/UD7/src/ACT7_1C/Main.java
@@ -9,10 +9,16 @@ public class Main {
     public static void main(String[] args) {
         Manager manager = new Manager("Barbara Johnson", "Manager", "1");
         TechnicalWriter tWriter = new TechnicalWriter("Bill Monroe", "Writer", "2");
+        GraphicIllustrator gIllustrator = new GraphicIllustrator("Jane Smith", "Illustrator", "2");
         manager.addEmployee(tWriter);
+        manager.addEmployee(gIllustrator);
+        
         tWriter.addSkill("Skill1");
         tWriter.addSkill("Skill2");
         tWriter.addSkill("Skill2");
+        
+        gIllustrator.addSkill("Skill1");
+        gIllustrator.addSkill("Skill2");
         
         System.out.println(manager.toString());
 
