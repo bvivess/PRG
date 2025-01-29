fcfad3799e873d9d61e2f5a3f30c2ef4be8cf0f2
unknown <bvives@iesemilidarder.com>
2025-01-29 12:53:22 +0100

    Initial

---
+++ UD07/UD7/src/ACT7_1C/Main.java
@@ -0,0 +1,14 @@
+package ACT7_1C;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Manager manager = new Manager("Barbara Johnson", "Manager", "US Marketing");
+
+    }
+    
+}
