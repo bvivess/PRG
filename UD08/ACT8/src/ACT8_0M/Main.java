9d24aea6ea2ad46ce6abf4033c9ae9333064b623
unknown <bvives@iesemilidarder.com>
2025-02-11 12:32:10 +0100

    Initial

--- UD08/ACT8/src/ACT8_0M/Main.java
+++ UD08/ACT8/src/ACT8_0M/Main.java
@@ -11,10 +11,16 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<FiguraGeometrica> figures = new ArrayList<FiguraGeometrica>();
+        
+        // FiguraGeometrica1
         FiguraGeometrica figuraGeometrica = new Cercle("Vermell", 5.0);
         figures.add(figuraGeometrica);
+        
+        // FiguraGeometrica2
         figuraGeometrica = new Quadrat("Groc", 5.0);
         figures.add(figuraGeometrica);
+        
+        // FiguraGeometrica3
         figuraGeometrica = new Rectangle("Verd", 5.0, 2.0);
         figures.add(figuraGeometrica);
         
