cce9cf74422e2071b40c7fb09988151ab781e006
bartomeu vives <bvives@iesemilidarder.com>
2025-02-24 17:36:26 +0100

    Initial

--- EXAMENS/EX2_2/src/EX2_2/Main.java
+++ EXAMENS/EX2_2/src/EX2_2/Main.java
@@ -5,23 +5,28 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<UsuariBase> usuaris = new ArrayList<UsuariBase>();
+        // 4.1
         Admin admin = new Admin("Pere","Martí","2");
         Client client = new Client("Joana","Garcia","Cerrer del pi");
         Moderador moderador = new Moderador("Aina","Mas", "Carrer del pla");
         
+        // 4.2
+        ArrayList<UsuariBase> usuaris = new ArrayList<UsuariBase>();
         usuaris.add(admin);
         usuaris.add(client);
         usuaris.add(moderador);
         
+        // 4.4
         for (UsuariBase u : usuaris)
             System.out.println(mostrarTipusUsuari(u));
 
+        // 3
         System.out.println(admin.metodePerDefecte());        
         System.out.println(client.metodePerDefecte());
         System.out.println(moderador.metodePerDefecte());
     }
     
+    // 4.3
     private static String mostrarTipusUsuari(UsuariBase usuari) {
         String text;
 
