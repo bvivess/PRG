dadde1f2ee7567faef5bb4ddb7bc4d82bc788381
bartomeu vives <bvives@iesemilidarder.com>
2025-03-04 16:07:01 +0100

    Initial

--- UD08/ACT8/src/ACT8_8/Camio.java
+++ UD08/ACT8/src/ACT8_8/Camio.java
@@ -2,14 +2,18 @@ package ACT8_8;
 
 // Classe Cotxe que hereta de Vehicle i implementa VehicleMotor
 class Camio extends VehicleMotor {
+    boolean esFrigorific;
 
-    public Camio(String matricula, String marca, String model, int any, double potencia) {
+    public Camio(String matricula, String marca, String model, int any, double potencia, boolean esFrigorific) {
         super(matricula, marca, model, any, potencia);
+        this.esFrigorific = esFrigorific;
     }
     
     @Override
     public double calculaImposts() {
-        return super.calculaImposts() + 5;
+ 
+        return super.calculaImposts() + (this.esFrigorific ? 5 : 0);
+
     }
     //@Override
     //public double calculaManteniment() {
