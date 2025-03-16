1c491fc5bb758616c83270fa82eb152a2cb69453
unknown <bvives@iesemilidarder.com>
2025-03-16 08:08:31 +0100

    Initial

--- UD11/ACT11/src/ACT11_0H/Main.java
+++ UD11/ACT11/src/ACT11_0H/Main.java
@@ -1,5 +1,6 @@
 package ACT11_0H;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Main {
@@ -7,7 +8,10 @@ public class Main {
         // Function
         Function<String, Integer> longitud = s -> s.length();
         System.out.println(longitud.apply("Hola")); // 4
-
+        
+        // BiFunction
+        BiFunction<Integer, Integer, Double> dividir = (numerador, divisor) -> Double.valueOf(numerador / divisor);
+        System.out.println(dividir.apply(8,2));
     }
 }
 
---
+++ UD11/ACT11/src/ACT11_0I/Main.java
@@ -0,0 +1,15 @@
+package ACT11_0I;
+
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        // SUPPLIER
+        Supplier<Double> numAleatori = () -> Math.random();
+        System.out.println(numAleatori.get());  // Ex: 0.75432
+        
+        Supplier<Persona> personaGenerator = () -> new Persona();
+        Persona p = personaGenerator.get();
+    }
+}
+
