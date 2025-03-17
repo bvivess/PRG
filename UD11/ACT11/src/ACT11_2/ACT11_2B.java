3eca6e71dc1d46116838385edb5a45bce95f85e3
unknown <bvives@iesemilidarder.com>
2025-03-17 10:43:21 +0100

    Initial

---
+++ UD11/ACT11/src/ACT11_2/ACT11_2B.java
@@ -0,0 +1,18 @@
+package ACT11_2;
+
+
+import java.util.*;
+import java.util.stream.*;
+
+public class ACT11_2B {
+    public static void main(String[] args) {
+        List<String> noms = Arrays.asList("Juan", "Jordi", "Joan", "Pere", "Maria", "Joaquim");
+
+        noms.stream()
+            .filter(n -> n.startsWith("J")) // Filtrar noms que comencen amb "J"
+            .skip(2) // Saltar els primers 2 noms
+            .limit(3) // Mostrar només els primers 3 noms
+            .forEach(System.out::println); // Mostrar el resultat
+    }
+}
+
---
+++ UD11/ACT11/src/ACT11_2/ACT11_2C.java
@@ -0,0 +1,20 @@
+package ACT11_2;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class ACT11_2C {
+    public static void main(String[] args) {
+        List<String> noms = Arrays.asList("Juan", "Jordi", "Joan", "Pere", "Maria", "Joaquim");
+
+        boolean algunAmbA = noms.stream()
+            .anyMatch(n -> n.contains("a")); // Comprovar si hi ha algun nom amb "a"
+        
+        long nomsLlarg = noms.stream()
+            .filter(n -> n.length() > 4) // Filtrar noms amb més de 4 lletres
+            .count(); // Comptar el nombre de noms
+
+        System.out.println("Hi ha algun nom amb la lletra 'a': " + algunAmbA);
+        System.out.println("Noms amb més de 4 lletres: " + nomsLlarg);
+    }
+}
