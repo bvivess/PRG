f4d866cbf6ff83574c92a78664b62bbd46b12277
unknown <bvives@iesemilidarder.com>
2025-03-31 14:16:52 +0200

    Initial

--- UD11/UD11/src/ACT11_2/Main2.java
+++ UD11/UD11/src/ACT11_2/Main2.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class Main2 {
     public static void main(String[] args) {
-        String arxiu = "C:\\temp\\ACT11_1.cfg";
+        String arxiu = "C:\\temp\\ACT11_2.cvs";
         Map<Integer, Persona> persones = new HashMap<>();
 
         try ( BufferedReader bufferedReader = new BufferedReader(new FileReader(arxiu));
