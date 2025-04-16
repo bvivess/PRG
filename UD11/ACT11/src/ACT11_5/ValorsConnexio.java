d1c9d7b3cec893086306db2cde7b041364360067
unknown <bvives@iesemilidarder.com>
2025-04-16 07:16:03 +0200

    Initial

--- UD11/ACT11/src/ACT11_5/ValorsConnexio.java
+++ UD11/ACT11/src/ACT11_5/ValorsConnexio.java
@@ -1,18 +1,9 @@
 package ACT11_5;
 
 public enum ValorsConnexio {
-    SERVER ("SERVER"),
-    DBASE ("DBASE"),
-    USER ("USER"),
-    PASSWD ("PASSWD");
-    
-    private final String abreviacio; // atribut
-
-    ValorsConnexio(String abreviacio) { // constructor
-        this.abreviacio = abreviacio;
-    }
-    public String getAbreviacio() { // getter
-        return this.abreviacio;
-    }
+    SERVER,
+    DBASE,
+    USER,
+    PASSWD;
 
 }
