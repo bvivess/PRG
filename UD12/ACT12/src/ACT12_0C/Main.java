760a2ea1a0b1cbf62a58a710176ff921a002b522
unknown <bvives@iesemilidarder.com>
2025-04-30 07:09:36 +0200

    Initial

--- UD12/ACT12/src/ACT12_0C/Main.java
+++ UD12/ACT12/src/ACT12_0C/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // Classe anònima a partir d'Interfície
-        new Persona(){
+        new Persona() {
+            private String nom = "Bernat";
+            private int edat = 23;
             
             @Override
             public void saluda(){ 
-                System.out.println("Hola, sóc una persona anònima");
+                System.out.println("Hola, sóc " + this.nom + " una persona anònima");
             }
             
         }.saluda();
