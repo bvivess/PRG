bf736ce931aefa4a199b64ebd6cebd8b1c8494f2
bartomeu vives <bvives@iesemilidarder.com>
2025-05-06 14:48:26 +0200

    Initial

--- UD12/ACT12/src/ACT12_1B/ACT12_2A.java
+++ UD12/ACT12/src/ACT12_1B/ACT12_2A.java
@@ -1,20 +1,25 @@
 package ACT12_1B;
 
-import ACT12_1A.*;
 import java.util.*;
 import java.util.stream.*;
 
 public class ACT12_2A {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 5, 6, 7, 8, 9);
+        List<Persona> persones = Arrays.asList( new Persona("Juan", 1),
+                                                new Persona("Jordi",2),
+                                                new Persona("Joan", 3),
+                                                new Persona("Pere", 4),
+                                                new Persona("Maria", 5),
+                                                new Persona("Joaquim", 6),
+                                                new Persona("Jaume", 7),
+                                                new Persona("Judith", 8) );
 
-        List<Integer> resultat = numeros.stream()
-                                        .filter(n -> n % 2 != 0) // Filtrar nombres imparells
-                                        .map(n -> n * 2) // Multiplicar per 2
+        List<Persona> resultat = persones.stream()
+                                        .filter(p -> p.getEdat() % 2 != 0) // Filtrar nombres imparells
                                         .distinct() // Eliminar duplicats
                                         .collect(Collectors.toList()); // Recollir el resultat en una llista
 
-        System.out.println("Llista: " + numeros);
+        System.out.println("Llista: " + persones);
         System.out.println(resultat); // Mostrar el resultat
     }
 }
