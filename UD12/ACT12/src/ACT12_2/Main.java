d6aa3cbafdea96a8a52c7e7e9643e35043f0b348
unknown <bvives@iesemilidarder.com>
2025-05-06 08:23:14 +0200

    Initial

--- UD12/ACT12/src/ACT12_2/Main.java
+++ UD12/ACT12/src/ACT12_2/Main.java
@@ -4,13 +4,14 @@ import java.util.stream.Stream;
 
 public class Main{
     public static void main(String[] args) {
-       
 
         try (Stream<String> linies = Files.lines(Paths.get("c:\\temp\\clients.csv"))) {
+            
             linies
                 .filter(linia -> !linia.isBlank())
                 .map(String::toUpperCase)
                 .forEach(System.out::println);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
