87b0b64c8baad247ed779219694b6c7e2f04e7ef
unknown <bvives@iesemilidarder.com>
2025-05-07 07:09:21 +0200

    Initial

--- UD12/ACT12/src/ACT12_1A/ACT12_2I.java
+++ UD12/ACT12/src/ACT12_1A/ACT12_2I.java
@@ -5,7 +5,7 @@ import java.util.stream.*;
 
 public class ACT12_2I {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, -2, 3, -4, 5);
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
         boolean capNegatiu = numeros.stream()
                                     .noneMatch(n -> n < 0); // Comprovar si cap número és negatiu
@@ -14,10 +14,18 @@ public class ACT12_2I {
                                          .filter(n -> n > 0) // Filtrar només els números positius
                                          .map(n -> n * 10) // Multiplicar per 10
                                          .collect(Collectors.toList()); // Recollir en una llista
+        
+ 
+
+        double suma = numeros.stream().mapToDouble(n->n).sum();
+        int max = numeros.stream().mapToInt(n->n).max().orElse(0);
+        int min = numeros.stream().mapToInt(n->n).min().orElse(0);
+        double avg = numeros.stream().mapToInt(n->n).average().orElse(0);
 
         System.out.println("Llista: " + numeros);
         System.out.println("Cap numero es negatiu: " + capNegatiu);
         System.out.println("Resultat multiplicat per 10: " + resultats);
+        System.out.println("Suma/ Màxim / Mínim / Mitjana: " + suma + " / " + max + " / " + min + " / " + avg);
     }
 }
 
