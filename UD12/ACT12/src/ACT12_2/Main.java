db21111ae4d8fb8088aa3f8e8232604752896dc9
Tomeu Vives <bvives@iesemilidarder.com>
2025-05-10 16:04:18 +0200

    Initial

--- UD12/ACT12/src/ACT12_2/Main.java
+++ UD12/ACT12/src/ACT12_2/Main.java
@@ -1,19 +1,20 @@
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-
         try (Stream<String> linies = Files.lines(Paths.get("c:\\temp\\clients.csv"))) {
-            
+
             linies.filter(linia -> !linia.isBlank() && !linia.startsWith("#"))
                   .map(linia -> linia.split(","))
-                  .forEach(parts -> System.out.println("ID: " + parts[0] +  " Nom: " + parts[1] +  " Email: " + parts[2]));
-            
+                  .sorted((parts1, parts2) -> parts1[1].compareTo(parts2[1])) // ordenat per nom
+                  .forEach(parts -> System.out.println("ID: " + parts[0] + " Nom: " + parts[1] + " Email: " + parts[2]));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
     }
 }
+
