3b212af2a22a8b5789fa730d09130db42df2e429
unknown <bvives@iesemilidarder.com>
2025-05-12 07:55:47 +0200

    Initial

--- UD12/ACT12/src/ACT12_3/Main.java
+++ UD12/ACT12/src/ACT12_3/Main.java
@@ -16,15 +16,21 @@ public class Main {
 
         // Connexió a la base de dades
         try ( Connection conn = gestorBBDD.getConnectionFromFile() ) {
-
-            String sql = "INSERT INTO usuaris (id, nom) VALUES (?, ?)";
-
             names.entrySet().stream()
                  .forEach( tupla -> { 
                                      try {
-                                            gestorBBDD.executaSQL(conn, sql, tupla.getKey(), tupla.getValue());
+                                            gestorBBDD.executaSQL(conn, "INSERT INTO usuaris (id, nom) VALUES (?, ?)",
+                                                                  tupla.getKey(), tupla.getValue());
                                          } catch (SQLException e) {
-                                            System.err.println(e.getMessage());
+                                             try {
+                                                if (e.getSQLState().equals("23000") && e.getErrorCode() == 1062)
+                                                    gestorBBDD.executaSQL( conn, "UPDATE usuaris SET nom = ? WHERE id = ?",
+                                                                           tupla.getValue(), tupla.getKey() );
+                                                else 
+                                                    throw new RuntimeException (e);
+                                             } catch (SQLException e2) {
+                                                 throw new RuntimeException (e2);
+                                             }
                                          }
                                     }
                          );
