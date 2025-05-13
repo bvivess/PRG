022eea30773739b660b64a29ea113a930a179192
unknown <bvives@iesemilidarder.com>
2025-05-13 08:08:28 +0200

    Initial

---
+++ UD12/ACT12/src/ACT12_2/Client.java
@@ -0,0 +1,75 @@
+package ACT12_2;
+
+public class Client implements Comparable<Client>{
+    int id;
+    String nom;
+    String email;
+
+    public Client(int id, String nom, String email) {
+        setId(id);
+        setNom(nom);
+        setEmail(email);
+    }
+
+    @Override
+    public int compareTo(Client c) {
+        if (this.id > c.id) return 1;
+        else if (this.id < c.id) return -1;
+        else return 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        //if (nom == null || nom.isBlank())
+        //    throw new IllegalArgumentException ("Client.Nom incorrecte");
+        //else
+            this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Client other = (Client) obj;
+        return this.id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + ", nom=" + nom + ", email=" + email + '}';
+    }
+
+}
--- UD12/ACT12/src/ACT12_2/Main.java
+++ UD12/ACT12/src/ACT12_2/Main.java
@@ -1,16 +1,28 @@
+package ACT12_2;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         try (Stream<String> linies = Files.lines(Paths.get("c:\\temp\\clients.csv"))) {
 
-            linies.filter(linia -> !linia.isBlank() && !linia.startsWith("#"))
+            List<Client> clients = 
+                   linies.filter(linia -> !linia.isBlank() && !linia.startsWith("#"))
                   .map(linia -> linia.split(","))
                   .sorted((parts1, parts2) -> parts1[1].compareTo(parts2[1])) // ordenat per nom
-                  .forEach(parts -> System.out.println("ID: " + parts[0] + " Nom: " + parts[1] + " Email: " + parts[2]));
+                  .map(parts -> new Client(
+                                    Integer.parseInt(parts[0].trim()),
+                                    parts[1].trim(),
+                                    parts[2].trim()))
+                  .collect(Collectors.toList());
+            
+            // Imprimeix la llista
+            clients.forEach(System.out::println);
 
         } catch (IOException e) {
             e.printStackTrace();
