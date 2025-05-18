544aa5f4b4de930ebf83eae7b64169534fc36028
Tomeu Vives <bvives@iesemilidarder.com>
2025-05-18 07:41:44 +0200

    Initial

--- UD12/ACT12/src/ACT12_1A/ACT12_2A.java
+++ UD12/ACT12/src/ACT12_1A/ACT12_2A.java
@@ -5,12 +5,13 @@ import java.util.stream.*;
 
 public class ACT12_2A {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 5, 6, 7, 8, 9);
+        List<Integer> numeros = Arrays.asList(4, 5, 3, 9, 2, 5, 6, 1, 8, 7);
 
         List<Integer> resultat = numeros.stream()
                                         .filter(n -> n % 2 != 0) // Filtrar nombres imparells
                                         .map(n -> n * 2) // Multiplicar per 2
                                         .distinct() // Eliminar duplicats
+                                        .sorted()   // Ordenar-los
                                         .collect(Collectors.toList()); // Recollir el resultat en una llista
 
         System.out.println("Llista: " + numeros);
