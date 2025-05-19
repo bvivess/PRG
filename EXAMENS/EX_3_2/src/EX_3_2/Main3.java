7f60322c780d6cfa753c89c171d31500b187dbda
unknown <bvives@iesemilidarder.com>
2025-05-19 20:15:16 +0200

    Initial

--- EXAMENS/EX_3_2/src/EX_3_2/Main3.java
+++ EXAMENS/EX_3_2/src/EX_3_2/Main3.java
@@ -11,11 +11,13 @@ public class Main3 {
 
         // Llista de vehicles
         List<Vehicle> vehicles = Arrays.asList(
-            new Vehicle("1234-HJK", "Toyota", "Corolla", 2010, 12000),
-            new Vehicle("5678-KLM", "Volkswagen", "Golf", 2018, 18000),
-            new Vehicle("9012-NPR", "Seat", "Ibiza", 2015, 10000),
-            new Vehicle("3456-STU", "Tesla", "Model 3", 2022, 40000),
-            new Vehicle("7890-VWX", "Renault", "Clio", 2012, 9000)
+            new Vehicle("1234-HJK", "Toyota",     "Corolla", 2010, 12000),
+            new Vehicle("1230-BCN", "Toyota",     "Yaris",   2010, 12200),
+            new Vehicle("5678-KLM", "Volkswagen", "Golf",    2018, 18000),
+            new Vehicle("5669-HYM", "Volkswagen", "Polo",    2020, 21000),
+            new Vehicle("9012-NPR", "Seat",       "Ibiza",   2015, 10000),
+            new Vehicle("3456-STU", "Tesla",      "Model 3", 2022, 40000),
+            new Vehicle("7890-VWX", "Renault",    "Clio",    2012,  9000)
         );
 
         // Lambda que incrementa el preu del vehicle un 10%, modificant l'objecte
