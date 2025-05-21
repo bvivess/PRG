f6a9aa388121e1b33e405f981039b7e7828280a9
unknown <bvives@iesemilidarder.com>
2025-05-21 16:09:31 +0200

    Initial

---
+++ UD12/ACT12/src/ACT12_2C/Department.java
@@ -0,0 +1,52 @@
+package ACT12_2C;
+
+import ACT12_2A.*;
+import java.util.Objects;
+
+public class Department  implements Comparable<Department> {
+    String id;
+
+    public Department(String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public int compareTo(Department d) {
+        return this.compareTo(d);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Department other = (Department) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return "Department{" + "id=" + id + '}';
+    }
+    
+}
