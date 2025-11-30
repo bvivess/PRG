ae6d62caa89c7792f64d05d4041760502cb6dfa9
unknown <bvives@iesemilidarder.com>
2025-11-30 08:10:06 +0100

    Initial

--- EXAMENS/EX_1_2/src/EX_1_2/UtilExamen.java
+++ EXAMENS/EX_1_2/src/EX_1_2/UtilExamen.java
@@ -49,18 +49,21 @@ public class UtilExamen {
     public static ArrayList<Integer> ordenaArray(int[] array) {
         ArrayList<Integer> resultat = new ArrayList<>();
         
-        if (array.length > 0) resultat.add(array[0]);
-        for (int i=1; i<array.length; i++) {
-            boolean trobat = false;
-            for (int j=0; j<resultat.size(); j++) {
-                if (array[i] < resultat.get(j))  {
-                    resultat.add(j, array[i]);  // afegir en la posició 'j' de 'resultat'
-                    trobat = true;
-                    break;
+        if (array.length > 0) {
+            resultat.add(array[0]);
+
+            for (int i=1; i<array.length; i++) {
+                boolean trobat = false;
+                for (int j=0; j<resultat.size(); j++) {
+                    if (array[i] < resultat.get(j))  {
+                        resultat.add(j, array[i]);  // afegir en la posició 'j' de 'resultat'
+                        trobat = true;
+                        break;
+                    }
                 }
-            }
-            if (!trobat) {
-                resultat.add(array[i]);  // afegir al final de 'resultat'
+                if (!trobat) {
+                    resultat.add(array[i]);  // afegir al final de 'resultat'
+                }
             }
         }
         return resultat;
