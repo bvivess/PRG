309744366ccc0de89b302f645cabfb5261c99960
bartomeu vives <bvives@iesemilidarder.com>
2026-02-12 16:24:06 +0100

    Initial

--- UD08/ACT8/src/ACT8_0E/Classe2.java
+++ UD08/ACT8/src/ACT8_0E/Classe2.java
@@ -8,13 +8,13 @@ class Classe2 implements Interficie {
     
     @Override
     public void metodeAbstracte() {
-        System.out.println("MÈTODE ABSTRACTE en Classe2: " + a1);
+        System.out.println("MÈTODE ABSTRACTE en Classe2: " + this.a1);
     }
     
     // La Classe2 sobreescriu el metodePerDefecte() de la interfície
     @Override
     public void metodePerDefecte() {
-        System.out.println("MÈTODE PER DEFECTE en Classe2: " + a1);
+        System.out.println("MÈTODE PER DEFECTE en Classe2: " + this.a1);
     }
     
 
