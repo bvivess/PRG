f2dec293235dbb892e6cfbcf279ece9efeb9f301
unknown <bvives@iesemilidarder.com>
2026-02-12 20:26:41 +0100

    Initial

--- UD08/ACT8/src/ACT8_0E/Classe2.java
+++ UD08/ACT8/src/ACT8_0E/Classe2.java
@@ -8,13 +8,13 @@ class Classe2 implements Interficie {
     
     @Override
     public void metodeAbstracte() {
-        System.out.println("MÈTODE ABSTRACTE en Classe2: " + this.a1);
+        System.out.println("MÈTODE ABSTRACTE en Classe2: " + Interficie.a1);  // també 'this.a1'
     }
     
     // La Classe2 sobreescriu el metodePerDefecte() de la interfície
     @Override
     public void metodePerDefecte() {
-        System.out.println("MÈTODE PER DEFECTE en Classe2: " + this.a1);
+        System.out.println("MÈTODE PER DEFECTE en Classe2: " + this.a1);  // també 'Interficie.a1'
     }
     
 
