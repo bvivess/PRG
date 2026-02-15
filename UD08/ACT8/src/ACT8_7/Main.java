dcae05838ad607e645281ac9e042b58c8929f38d
unknown <bvives@iesemilidarder.com>
2026-02-15 10:06:58 +0100

    Initial

--- UD08/ACT8/src/ACT8_7/Main.java
+++ UD08/ACT8/src/ACT8_7/Main.java
@@ -8,22 +8,21 @@ public class Main {
 
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
-        // Crear un client de la biblioteca
+        // 1. Crear un client de la biblioteca
         Client client = new Client("Anna Martínez");
         biblioteca.afegeixClient(client);
-
-        // Afegir alguns llibres a la biblioteca
+        // 2. Afegir alguns llibres a la biblioteca
         biblioteca.afegeixLlibreDisponible(new Llibre("El Senyor dels Anells", "J.R.R. Tolkien", 1954, 25.0));
         biblioteca.afegeixLlibreDisponible(new Llibre("Cien a?os de soledad", "Gabriel García Márquez", 1967, 20.0));
         biblioteca.afegeixLlibreDisponible(new Llibre("1984", "George Orwell", 1949, 18.0));
-        // Mostrar els llibres de la biblioteca
+        // 3. Mostrar els llibres de la biblioteca
         System.out.println(biblioteca.toString());
         
         System.out.println("------------------------------");
         
-        // Presta llibre
+        // 4. Presta llibre
         biblioteca.prestaLlibre("Anna Martínez", "El Senyor dels Anells");
-        // Mostrar els llibres de la biblioteca
+        // 5. Mostrar els llibres de la biblioteca
         System.out.println(biblioteca.toString());
     }
 }
