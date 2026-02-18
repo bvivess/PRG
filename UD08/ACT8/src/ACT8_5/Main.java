414aba8bb484c7558b967c8909f266feb28dd9de
bartomeu vives <bvives@iesemilidarder.com>
2026-02-18 19:30:00 +0100

    Initial

--- UD08/ACT8/src/ACT8_5/Main.java
+++ UD08/ACT8/src/ACT8_5/Main.java
@@ -29,14 +29,14 @@ public class Main {
         compteEstalvis.afegeixTarjeta(new Credit("000-0000-0000-0001",0,1000,1000,0));
         compteEstalvis.afegeixTarjeta(new Credit("000-0000-0000-0001",0,1000,1000,0));  // Duplicada, no l'afegeix
         compteEstalvis.afegeixTarjeta(new Debit("000-0000-0000-0002",0,1000,1000,0));
-        compteEstalvis.eliminaTarjeta("000-0000-0000-0002");
+        compteEstalvis.eliminaTarjeta("000-0000-0000-0002");  // Eliminada
         
         Persona persona = new Persona("Joana Moragues", 27);
         persona.afegeixProducteBancari(compteEstalvis);
         persona.afegeixProducteBancari(new Diposit("0001-0001-0002", "Deposit1", 100, 2.5, 10));
         persona.afegeixProducteBancari(new Hipoteca("0001-0001-0003", "Hipoteca1", 100, 5, 1, 10));
         persona.afegeixProducteBancari(new Hipoteca("0001-0001-0003", "Hipoteca1", 100, 5, 1, 10));  // Duplicada, no l'afegeix
-        persona.eliminaProducteBancari("0001-0001-0003");
+        persona.eliminaProducteBancari("0001-0001-0003");  // Eliminada
         persona.afegeixProducteBancari(new FonsInversio("0001-0001-0004", "FonsInversio1", 100, 5, 1, 10));
         
         System.out.println(persona.toString() + "\n Total Remuneració: " + persona.calculaRemuneracioTotal());
