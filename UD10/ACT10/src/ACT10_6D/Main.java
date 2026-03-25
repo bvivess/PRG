e3cb514700bf5b4be4a53bc865fd1104d01dd299
bvivess <bvives@iesemilidarder.com>
2026-03-25 08:29:44 +0100

    Initial

--- UD10/ACT10/src/ACT10_6D/Main.java
+++ UD10/ACT10/src/ACT10_6D/Main.java
@@ -9,11 +9,11 @@ public class Main {
         ArrayMax<Tasca> tasques = new ArrayMax<>(5);  // nombre màxim d'elements permès? 5
         
         // Prova de 'ArrayMax'
-        tasques.add(new Tasca(LocalDateTime.now(), "Anar al metge"));
         tasques.add(new Tasca(LocalDateTime.now(), "Anar a classe"));
         tasques.add(new Tasca(LocalDateTime.now(), "Comprar menjar"));
         tasques.add(new Tasca(LocalDateTime.now(), "Fer els ejercicis"));
         tasques.add(new Tasca(LocalDateTime.now(), "Sopar"));
+        tasques.add(new Tasca(LocalDateTime.now(), "Llegir un llibre"));
         tasques.add(new Tasca(LocalDateTime.now(), "Domir"));  // Error: no s'admeten més de 5 tasques
         
         for (Tasca t : tasques) {
