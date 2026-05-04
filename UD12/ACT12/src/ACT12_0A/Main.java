d74085d2ff4ecff0aae3722527df2a9348102e5a
bvivess <bvives@iesemilidarder.com>
2026-05-04 12:24:03 +0200

    Initial

--- UD12/ACT12/src/ACT12_0A/Main.java
+++ UD12/ACT12/src/ACT12_0A/Main.java
@@ -4,14 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Classe anònima a partir de Classe simple
-        new Persona("Bernat", 23) {
-            
-            @Override
-            public void saluda(){ 
-                System.out.println("Hola, sóc " + super.getNom() + ", una persona anònima");
-            }
-            
-        }.saluda();
+        new Persona("Bernat", 23) {  }.saluda();
     }
     
 }
